693a855427095a80c62f5e0d8eea03cf7ea6b05a
Adrian <adfpizarro@gmail.com>
2020-06-15 17:05:27 -0600

    Changed length for size, recommendation of the stand uo team

--- enumerables.rb
+++ enumerables.rb
@@ -3,12 +3,12 @@ has= {"One" => 1,"Two" => 2,"Three" => 3}
 
 def my_each
   return enum_for(__callee__) unless block_given?
-  length.times {|i| yield to_a[i]}
+  size.times {|i| yield to_a[i]}
 end
 
 def my_each_with_index
   return enum_for(__callee__) unless block_given?
-  length.times {|i| yield to_a[i]}
+  size.times {|i| yield to_a[i]}
 end
 
 arr.my_each_with_index
