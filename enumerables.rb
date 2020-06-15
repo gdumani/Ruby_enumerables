584719c99d06964d397d5269132c5c893fe4c9c8
Adrian <adfpizarro@gmail.com>
2020-06-15 16:57:05 -0600

    Added the function name

--- enumerables.rb
+++ enumerables.rb
@@ -1,8 +1,14 @@
 arr = [0,1,2,3,4,5]
+has= {"One" => 1,"Two" => 2,"Three" => 3}
 
 def my_each
   return enum_for(__callee__) unless block_given?
   length.times {|i| yield to_a[i]}
 end
 
-arr.my_each
+def my_each_with_index
+  return enum_for(__callee__) unless block_given?
+  length.times {|i| yield to_a[i]}
+end
+
+arr.my_each_with_index
