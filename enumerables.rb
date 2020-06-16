42514f55a53f9cd3e6865c7bb392de2876543370
gdumani <gdumani@gmail.com>
2020-06-16 09:36:55 -0600

    created my_each_with_index method added to test arrays, one with strings and one with integers

--- enumerables.rb
+++ enumerables.rb
@@ -1,8 +1,13 @@
-arr = [0,1,2,3,4,5]
+ars = %w[ab cd ef ij kx]
+ari = [10, 20, 30, 40, 50]
 
 def my_each
   return enum_for(__callee__) unless block_given?
   length.times {|i| yield to_a[i]}
 end
 
-arr.my_each
+
+def my_each_with_index
+  return enum_for(__callee__) unless block_given?
+  length.times {|i| yield(to_a[i], i)}
+end
