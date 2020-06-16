c8a3e20ddf928c2e9b5430c693b6e40f994ae513
Adrian <adfpizarro@gmail.com>
2020-06-16 10:10:00 -0600

    Added the my_select functiion

--- enumerables.rb
+++ enumerables.rb
@@ -12,3 +12,24 @@ def my_each_with_index
   length.times {|i| yield(to_a[i], i)}
 end
 
+def my_select
+  res=[]
+
+  self.my_each do |i|
+
+    if yield(i)
+     res << i
+    end
+  end
+
+  return res
+end
+
+public :my_each
+public :my_each_with_index
+public :my_select
+
+
+
+
+puts ari.my_select {|element| element > 20}
