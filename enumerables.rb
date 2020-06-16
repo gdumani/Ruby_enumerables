dcabd75b68dd4ee7137f2901f0ef6ef1df1db940
gdumani <gdumani@gmail.com>
2020-06-16 10:38:52 -0600

    created my_all method

--- enumerables.rb
+++ enumerables.rb
@@ -13,16 +13,26 @@ def my_each_with_index
 end
 
 def my_select
+  return enum_for(__callee__) unless block_given?
   res=[]
   self.my_each {|i| res << i if yield(i)}
   return res
 end
 
+def my_all
+  return enum_for(__callee__) unless block_given?
+  self.my_each {|i| return false  if !yield(i)} 
+  return true
+end
+  
+
 public :my_each
 public :my_each_with_index
 public :my_select
+public :my_all
 
 
 
 
-puts ari.my_select {|element| element > 20}
+puts ari.my_all {|e| e < 60}
+puts ari.my_all {|e| e > 60}
