32f85144984f57eea87a28dcd04e3551477429ef
Adrian <adfpizarro@gmail.com>
2020-06-18 09:13:32 -0600

    Fixed methods and updated readme file
    Updates method names for
    my_any > my_any?
    my_all > my_all?
    my_none > my_none?
    
    Updated the my_count method to accept parameters
    
    Simplified tehe my_each method eliminated nested ifs to reduce
    complexity

--- enumerables.rb
+++ enumerables.rb
@@ -8,7 +8,7 @@ module Enumerable
   def my_each_with_index
     return enum_for(__callee__) unless block_given?
 
-    length.times { |i| yield(to_a[i], i) }
+    size.times { |i| yield(to_a[i], i) }
   end
 
   def my_select
@@ -19,32 +19,36 @@ module Enumerable
     res
   end
 
-  def my_all
+  def my_all?
     return enum_for(__callee__) unless block_given?
 
     my_each { |i| return false unless yield(i) }
     true
   end
 
-  def my_any
+  def my_any?
     return enum_for(__callee__) unless block_given?
 
     my_each { |i| return true if yield(i) }
     false
   end
 
-  def my_none
+  def my_none?
     return enum_for(__callee__) unless block_given?
 
     my_each { |i| return false if yield(i) }
     true
   end
 
-  def my_count
-    return enum_for(__callee__) unless block_given?
-
+  def my_count(cond = nil)
     c = 0
-    my_each { |i| c += 1 if yield(i) }
+    if block_given?
+
+      my_each { |i| c += 1 if yield(i) }
+    else
+      my_each { |i| c += 1 if i == cond }
+
+    end
     c
   end
 
@@ -58,35 +62,37 @@ module Enumerable
     res
   end
 
-  # rubocop: disable Metrics/PerceivedComplexity
   def my_inject(acumulator = nil, operator = nil)
     if block_given?
       if acumulator.nil?
         acumulator = to_a[0]
-        (1...length).my_each { |i| acumulator = yield(acumulator, to_a[i]) }
+        (1...size).my_each { |i| acumulator = yield(acumulator, to_a[i]) }
       else
         my_each { |i| acumulator = yield(acumulator, i) }
       end
+    elsif operator.nil?
+      operator = acumulator
+      acumulator = to_a[0]
+      (1...size).my_each { |i| acumulator = acumulator.method(operator).call(to_a[i]) }
     else
-      if operator.nil?
-        operator = acumulator
-        acumulator = nil
-      end
-      if acumulator.nil?
-        acumulator = to_a[0]
-        (1...length).my_each { |i| acumulator = acumulator.method(operator).call(to_a[i]) }
-      else
-        my_each { |i| acumulator = acumulator.method(operator).call(i) }
-      end
+      my_each { |i| acumulator = acumulator.method(operator).call(i) }
+
     end
     acumulator
   end
-  # rubocop: enable Metrics/PerceivedComplexity
 
   def multiply_els
     my_inject(:*)
   end
 end
+
+# Test cases to review
+# puts (5..10).my_inject {|sum, n| sum + n}
+# puts [1,2,4,2].my_count(2)
+
+# puts %w[ant bear cat].my_all? {|word| word.length >= 3}
+# puts %w[ant bear cat].my_none? {|word| word.length == 5}
+# puts %w[ant bear cat].my_any? {|word| word.length >= 5}
 
 # #------test data-------
 # ars = %w[ab cd ef ij kx]
